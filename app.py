07df01121ceac6a8136a867c35581bbf8513faf3
jisuuuuu <115731230+jisuuuuu@users.noreply.github.com>
2024-06-02 20:10:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,9 @@ def process_image():
     np_array = np.frombuffer(image_bytes, np.uint8)
     frame = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
     
+    counter = data.get('counter', 0)
+    stage = data.get('stage', None)
+
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = pose.process(image)
@@ -42,7 +45,32 @@ def process_image():
 
     landmarks_list = [{"x": lm.x, "y": lm.y} for lm in landmarks]
 
-    return jsonify({"landmarks": landmarks_list})
+    try:
+        shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+        elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+        wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+        hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x, landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+        knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x, landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+        ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+
+        angle_knee = calculate_angle(hip, knee, ankle)
+        
+        if angle_knee > 169:
+            stage = "UP"
+        if stage == "UP" and 80 <= angle_knee <= 100:
+            stage = "Perfect"
+            counter += 1
+        elif stage == "UP" and (70 <= angle_knee < 80 or 100 < angle_knee <= 110):
+            stage = "Good"
+            counter += 1
+        elif stage == "UP" and (60 <= angle_knee < 70 or 110 < angle_knee <= 120):
+            stage = "Soso"
+
+    except:
+        pass
+
+    return jsonify({"landmarks": landmarks_list, "counter": counter, "stage": stage})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
